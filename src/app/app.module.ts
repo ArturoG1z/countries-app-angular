71ddc8620fd48f7a9f8b414f9b8689e221968edb
Felipe Arturo Glz <arturoglezggg@gmail.com>
2022-05-09 15:12:04 -0500

    Created modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { CountryModule } from './country/country.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    CountryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
