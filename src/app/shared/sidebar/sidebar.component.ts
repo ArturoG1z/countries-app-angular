b9eb5e4497afa55e868912b8c535bd71fd1d011f
Felipe Arturo Glz <arturoglezggg@gmail.com>
2022-05-09 17:55:37 -0500

    Added debounceTime for clicking on responsive

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,17 +7,32 @@ import { Component, ElementRef, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  private rxjsSimulator: Subject<number> = new Subject();
+  private subscription: Subscription = new Subscription();
 
-  constructor(private elRef: ElementRef) { }
+  constructor(private elRef: ElementRef) {
+    this.subscription.add(
+      // Here you can set your debounce time
+      this.rxjsSimulator.pipe(debounceTime(70)).subscribe(() => {
+          this.triggerHideNav();
+        }
+      )
+    );
+  }
 
   ngOnInit(): void {
   }
 
-  hideNav(): void {
-    if (this.elRef.nativeElement.visibility === 'visible') return;
+  triggerHideNav(): void {
     this.elRef.nativeElement.classList.remove('active');
   }
+  
+  hideNav(): void {
+    if (this.elRef.nativeElement.visibility === 'visible') return;
+    this.rxjsSimulator.next(1);
+  }
 
+  
   toggleNav(): void {
     if (this.elRef.nativeElement.visibility === 'visible') return;
     this.elRef.nativeElement.classList.toggle('active');
